09c1e94c1d53c36ded1b2e01c283e8df618f8ba8
Christian BeauxYeux <gildweek@gmail.com>
2024-07-25 22:05:19 +0400

    Add Auth to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_user
+
+  private
+
+  def authenticate_user
+    token = request.headers['Authorization']&.split(' ')&.last
+    @current_user = User.find_by(token: token)
+
+    unless @current_user
+      render json: { error: 'Unauthorized' }, status: :unauthorized
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
+  before_create :generate_token
   has_many :posts
+
+  private
+
+  def generate_token
+    self.token = SecureRandom.hex(16) if self.token.blank?
+  end
 end
