896cb0659f92e4728c7912814738cb08603dc907
Christian BeauxYeux <gildweek@gmail.com>
2024-07-25 22:08:26 +0400

    Create controllers and views for users and posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/edit'
+  get 'posts/update'
+  get 'posts/show'
+  get 'users/new'
+  get 'users/create'
+  get 'users/show'
 
   namespace :api do 
     namespace :v1 do
