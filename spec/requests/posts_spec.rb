0efe50dc4bfd589f92821f7311959dd2d990cc79
Christian BeauxYeux <gildweek@gmail.com>
2024-07-25 22:33:24 +0400

    Modif tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -4,17 +4,17 @@ RSpec.describe "Posts API", type: :request do
   let(:user) { User.create(username: "testuser", password: "password", token: "valid_token") }
   let(:headers) { { "Authorization" => "Bearer #{user.token}" } }
 
-  describe "GET /api/v1/posts" do
+  describe "GET /api/v1/users/:user_id/posts" do
     it "returns all posts" do
-      get "/api/v1/posts", headers: headers
+      get "/api/v1/users/#{user.id}/posts", headers: headers
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "POST /api/v1/posts" do
+  describe "POST /api/v1/users/:user_id/posts" do
     it "creates a new post" do
       post_params = { post: { content: "New post", user_id: user.id } }
-      post "/api/v1/posts", params: post_params, headers: headers
+      post "/api/v1/users/#{user.id}/posts", params: post_params, headers: headers
       expect(response).to have_http_status(:created)
     end
   end
